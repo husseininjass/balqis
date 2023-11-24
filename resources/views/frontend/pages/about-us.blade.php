3a4ecada8b3da6adf27bb260e46cc69a7861ec6e
husseininjass <hussein.husam7@gmail.com>
2023-11-24 20:47:05 +0300

    cart

--- resources/views/frontend/pages/about-us.blade.php
+++ resources/views/frontend/pages/about-us.blade.php
@@ -95,6 +95,9 @@
 		</div>
 	</section>
 	<!-- End Shop Services Area -->
-
-	{{-- @include('frontend.layouts.newsletter') --}}
+	<div class="map-section">
+		<div id="myMap">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d216.9835297624876!2d35.00879407983613!3d29.524039532876607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1500704ed4ce54db%3A0xb4df7f3f9099ab37!2z2LTYsdmD2Kkg2KjZhNmC2YrYsyDZhNmE2KrZg9mG2YjZhNmI2KzZitinINin2YTYrdiv2YrYq9mH!5e0!3m2!1sen!2sjo!4v1700845852089!5m2!1sen!2sjo" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		</div>
+	</div>
 @endsection
